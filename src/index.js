9495cce7d58478049b96f386fb77353aac1b2046
gargee-banerjee <gegemegargee@gmail.com>
2023-06-18 19:11:51 +0530

    NightSkyFolderUpdate

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import MainViewComponent from './components/MainViewComponent';
+import MainViewComponent from './TheNightSkyComponent/components/MainViewComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
